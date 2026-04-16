b6e6005be3261ed5985becaa0e319bbe37b226ef
Ainoa Dura Bordonado <ainoadura@outlook.com>
2026-04-16 09:54:09 +0200

    feat: configurar react-router-dom y rutas principales

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
-import './App.css'
+import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-200">
-      <div className="bg-red-500 p-10 text-white font-bold rounded-2xl shadow-2xl">
-        ¡Tailwind funcionando perfectamente! 🚀
-      </div>
+   <div className="min-h-screen bg-gray-100">
+      {/* Navbar sencillo de prueba */}
+      <nav className="bg-white p-4 shadow-md flex gap-4 justify-center">
+        <Link to="/" className="text-blue-500 hover:underline">Muro</Link>
+        <Link to="/biblioteca" className="text-blue-500 hover:underline">Biblioteca</Link>
+        <Link to="/add" className="text-blue-500 hover:underline">Añadir</Link>
+      </nav>
+
+      {/* Definición de qué se ve en cada ruta */}
+      <Routes>
+        <Route path="/" element={<div className="p-10 text-center text-2xl">📱 Aquí irá el Muro Social</div>} />
+        <Route path="/biblioteca" element={<div className="p-10 text-center text-2xl">📚 Aquí irá tu Biblioteca</div>} />
+        <Route path="/add" element={<div className="p-10 text-center text-2xl">➕ Formulario para añadir contenido</div>} />
+      </Routes>
     </div>
   )
 }
