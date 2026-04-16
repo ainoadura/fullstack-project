cfa297be4745f006e136ec4288571c73198d9f15
Ainoa Dura Bordonado <ainoadura@outlook.com>
2026-04-16 09:45:44 +0200

    feat: frontend totalmente funcional con Tailwind v4

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-200">
+      <div className="bg-red-500 p-10 text-white font-bold rounded-2xl shadow-2xl">
+        ¡Tailwind funcionando perfectamente! 🚀
+      </div>
+    </div>
+  )
+}
+
+export default App
